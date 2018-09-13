fffc01914f4b20bd0ba0081e7eb6669f1af3a15a
SunFlowerY1 <liupan_92@163.com>
2018-09-13 10:36:20 +0800

    awk 自定义输出分隔符

--- awk.sh
+++ awk.sh
@@ -1,3 +1,5 @@
 1、awk -F: '{ print "User", $1, "is really", $5}' /etc/passwd
 #print字段要加逗点，否则会被连接在一起
 2、awk -F: '{ printf "User %s is really %s\n", $1, $5}' /etc/passwd
+3、awk -F: -v 'OFS=**' '{ print $1, $5 }' /etc/passwd
+# 设置输出分隔符
